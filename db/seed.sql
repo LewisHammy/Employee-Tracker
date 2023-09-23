be5e1303bd190fc60e0f9a07bd67ba587cf97882
LewisHammy <samkabir30@gmail.com>
2023-09-23 00:28:27 -0700

    updated seeds.sql, downloaded table.module

--- db/seed.sql
+++ db/seed.sql
@@ -7,7 +7,7 @@ INSERT INTO role (title, salary, departmentId)
 VALUES ('Design Director', 90000, 1), ('Web Designer', 50000, 1), ('Print Designer', 40000, 1), ('Creative Director', 80000, 2), ('Marketing Manager', 70000, 2), ('Social Media Specialist', 40000, 2);
 
 INSERT INTO employee (firstName, lastName, roleId)
-VALUES ('Joseph', 'Carter Brown', 1), ('Megan', 'Johnson', 4), ('Stacy', 'Ruddy', 5);
+VALUES ('Samuel', 'Anita', 1), ('Blue', 'Color', 4), ('Daniel', 'Julia', 5);
 
 INSERT INTO employee (firstName, lastName, roleId, managerId)
-VALUES ('Lauren', 'Siminski', 2, 2), ('Jessica', 'Meyer', 3, 1), ('Rory', 'Nachbar', 6, 3);
+VALUES ('Caity', 'Cathreen', 2, 2), ('Daniela', 'Matthew', 3, 1), ('Pedro', 'Adrian', 6, 3);
